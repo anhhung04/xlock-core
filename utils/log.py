c112b53e06d444d9625124f6f514237739e4648f
Anh-Hung Hoang <86340766+anhhung04@users.noreply.github.com>
2024-07-25 21:07:55 +0700

    refractor: implement logger and dependency injection pattern (#17)
    * refractor code base
    
    * feat: add ci/cd
    
    * fix: invalid test
    
    * fix: invalid report
    
    * fix: exit code
    
    * redirect stderr to stdout
    
    * fix: gtihub action syntax
    
    * fix test exit code
    
    * style: migrate to new github action syntax
    
    * fix: deliver report content
    
    * fix syntax ci/cd
    
    * fix test script
    
    * fix test syntax for better report

--- utils/log.py
+++ utils/log.py
@@ -1,18 +1,30 @@
+import logging
 import structlog
+from structlog.stdlib import filter_by_level, add_logger_name, add_log_level
 
-from config import config
 
-processors: list = [
-    structlog.processors.add_log_level,
-    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S", utc=False),
-    structlog.processors.dict_tracebacks,
-]
+def configure_structlog():
+    structlog.configure(
+        processors=[
+            filter_by_level,  # Filter logs based on severity
+            add_logger_name,  # Add the logger name to the event
+            add_log_level,  # Add the log level to the event
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,  # Include exception details
+            structlog.dev.ConsoleRenderer(),  # Pretty logs in development
+        ],
+        context_class=structlog.threadlocal.wrap_dict(dict),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
 
-if config["PROD"]:
-    processors.append(structlog.processors.JSONRenderer())
-else:
-    processors.append(structlog.dev.ConsoleRenderer())
 
-structlog.configure(processors=processors)
+def disable_uvicorn_loggers():
+    logging.getLogger("uvicorn.access").handlers = []
+    logging.getLogger("uvicorn").handlers = []
 
-logger = structlog.get_logger()
+
+logger: structlog.PrintLogger = structlog.get_logger(__name__)

833ec41bc83fc16f5b3699d426f723c388bd0341
henrynguci <henrynguci@gmail.com>
2024-07-31 16:19:51 +0700

    implement share item dto

--- models/user.py
+++ models/user.py
@@ -1,12 +1,22 @@
-from pydantic import BaseModel, EmailStr, field_validator, Field
-from typing import Optional
-from models.response import BaseResponseModel
 from datetime import date, datetime
+from typing import Optional
+
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field,
+    field_validator,
+    model_validator,
+)
+
+from models.response import BaseResponseModel
+
 
 class RSAKeyPair(BaseModel):
     public: str
     enc_pri: str
     salt: str
+
 
 class GetUserDetail(BaseModel):
     id: str
@@ -15,12 +25,20 @@ class GetUserDetail(BaseModel):
     backup_email: Optional[EmailStr] = None
     fullname: str
     dob: str
-    address: str    
+    address: str
     phone_number: str
     country: str
     gender: str
-    created_at: str = Field(..., examples=["2024-08-16 00:00:00"], description="Date time in format YYYY-MM-DD HH:MM:SS")
-    updated_at: Optional[str] = Field(default=None, examples=["2024-08-16 00:00:00"], description="Date time in format YYYY-MM-DD HH:MM:SS")
+    created_at: str = Field(
+        ...,
+        examples=["2024-08-16 00:00:00"],
+        description="Date time in format YYYY-MM-DD HH:MM:SS",
+    )
+    updated_at: Optional[str] = Field(
+        default=None,
+        examples=["2024-08-16 00:00:00"],
+        description="Date time in format YYYY-MM-DD HH:MM:SS",
+    )
 
     @field_validator("created_at", "updated_at", mode="after")
     def time_format(cls, v):
@@ -29,35 +47,52 @@ class GetUserDetail(BaseModel):
         dt = datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f")
         return dt.strftime("%Y-%m-%d %H:%M:%S")
 
+
 class UserDetailResponse(BaseResponseModel):
     data: GetUserDetail
+
 
 class DeleteUserResponse(BaseResponseModel):
     data: None
 
+
 class UserDetailWithKey(GetUserDetail):
     rsa_key_pair: RSAKeyPair
+
 
 class UserDetailWithKeyResponse(BaseResponseModel):
     data: UserDetailWithKey
 
+
 class CreateUserModel(BaseModel):
-    username: str = Field(..., description="Display name of user", examples=["mr.xlock"])
+    username: str = Field(
+        ..., description="Display name of user", examples=["mr.xlock"]
+    )
     email: EmailStr
     password: str
-    fullname: str = Field(..., description="Full name of user", examples=["John Doe"])
+    fullname: str = Field(
+        ..., description="Full name of user", examples=["John Doe"]
+    )
     dob: date
-    address: str    
+    address: str
     phone_number: str
     country: str
-    gender: str 
+    gender: str
     backup_email: Optional[EmailStr] = None
     rsa_key_pair: RSAKeyPair
-    
+
+
 class QueryUserModel(BaseModel):
     username: Optional[str] = None
     id: Optional[str] = None
     email: Optional[EmailStr] = None
+
+    @model_validator(mode="after")
+    def check_null(cls, values):
+        if not any(values.values()):
+            raise ValueError("At least one field must be filled")
+        return values
+
 
 class UpdateUserModel(BaseModel):
     username: Optional[str] = None
@@ -67,4 +102,3 @@ class UpdateUserModel(BaseModel):
     phone_number: Optional[str] = None
     country: Optional[str] = None
     gender: Optional[str] = None
-
